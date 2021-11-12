d4d60b390da9b48cadcedf5f8220199c2dc252f2
Aimharman <49126394+Aimharman@users.noreply.github.com>
2021-11-12 16:07:51 +0530

    Add files via upload
    Added Bubble sort for integer sorting!

--- read.py
+++ read.py
@@ -3,7 +3,7 @@ import sys
 # Reading Data from a CSV file ---------- Without Pandas & Numpy
 
 delimit = ','
-infile = open('C:\\Users\\ankes\\Documents\\Python Scripts\\data.csv', 'r')
+infile = open('C:\\Users\\ankes\\Documents\\Python Scripts\\newfile.csv', 'r')
 import csv
 newdata = []
 rows = 0
@@ -21,12 +21,12 @@ infile.seek(0)
 ncol = len(next(infile))
 print("Total number of data items in list vector: ",ncol)
 #print(newdata)
-#print(newdata[4][5])
+print(newdata[4][5])
 
 # Copying Data from source CSV file to new CSV file
 
 rows = -1
-with open('newfile.csv','w',newline = '') as newfile:
+with open('dribble.csv','w',newline = '') as newfile:
     writer = csv.writer(newfile)
     for row in newdata:
         rows += 1
@@ -36,7 +36,7 @@ with open('newfile.csv','w',newline = '') as newfile:
 # Modify: Adding column or data to any row in data frame ----- Without Pandas & Numpy
 
 rows = 0    
-with open('newfile.csv','w',newline = '') as modfile:
+with open('dribble.csv','w',newline = '') as modfile:
     moder = csv.writer(modfile)
     for row in newdata:
         if rows == 0:
@@ -66,6 +66,45 @@ print("Last Row Reached!!")
 print("No. of columns in CSV: ", cols)
 print("No. of rows in CSV: ", rows)
 
-# Counting Rows in CSV ---------------------------- Without
+# Sorting Data -------- Bubble Sorting
 
+# First covert str dataframe to int :
+rows = 0
+cols = 0
+sort = []
+ordsort = []
+for row in newdata:
+    if rows < 100:    
+        rows += 1
+        #print (newdata[rows][62])
+        sort.append(int(newdata[rows][62]))
+
+print(sort)
+
+
+# Then implement sorting algorithm :
+
+item = 0   
+iter = 0
+iterate = 1
+counter = 0
+#print (sort[item+1], sort[item])s
+while iter < 4000 :
+    #print("MC")
+    if iterate > 0:
+        try:
+            trye = sort[item+1]
+        except IndexError as error:
+            item = 0
+            counter += 1
+    if sort[item] < sort[item+1] :
+        iterate = 1
+        temp = sort[item]
+        sort[item] = sort[item + 1]
+        sort[item + 1] = temp
+        #print(sort[item])
+    item += 1
+    iter += 1    
+
+print(sort)
 infile.close
