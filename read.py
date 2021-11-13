144da6082e5f3c2de41385966f2bece0a3b961c1
Aimharman <49126394+Aimharman@users.noreply.github.com>
2021-11-13 19:46:29 +0530

    Sorting updated.

--- read.py
+++ read.py
@@ -107,4 +107,46 @@ while iter < 4000 :
     iter += 1    
 
 print("\n\n",sort)
+
+# Optimized bubble sort algorithm for implementation
+
+item = 1
+for i in sort:
+    try:
+        temp = sort[item]
+    except IndexError as error:
+        print ("Length of List: ", item)
+    item += 1
+lenoflist = item - 2
+
+item = 0
+ite = 0
+iterate = 0
+incount = 1
+count = 0
+while incount > 0:
+    count += 1
+    ite = 0
+    incount = 0
+    if iterate > 0:
+        try:
+            temp = sort[item+1]
+        except IndexError as error:
+            item = 0 
+            print("Error")
+    while ite < lenoflist:
+        if sort[ite] < sort[ite+1]:
+            incount = 1
+            temp = sort[ite]
+            sort[ite] = sort[ite+1]
+            sort[ite+1] = temp
+        ite += 1
+    item = 0
+    iterate = 1
+
+print ("Sorted List: ",sort)
+print ("Iterator run: ",count, "times")
+
+
+
 infile.close
