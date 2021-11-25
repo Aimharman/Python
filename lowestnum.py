cf3eb12c6de3fb308b49140650f9ec77f8975c38
Aimharman <49126394+Aimharman@users.noreply.github.com>
2021-11-25 19:46:23 +0530

    Parallel piped sorting - can be implemented with mutex

--- lowestnum.py
+++ lowestnum.py
@@ -1,12 +1,39 @@
-lista = [80,70,60,40,33,22,5,9]
-rangel = len(lista)
-dribble_list = lista
+import csv
+Data_Frame = []
+Data_Frame1 = []
+infile = open('C:\\Users\\ankes\\Documents\\Python Scripts\\inp.csv', 'r')
+rows = 0
+colendcount = 0
+var = 0
+for row in csv.reader(infile, delimiter = ','):
+    try:
+        xyz = row[rows]
+    except IndexError:
+        if colendcount < 1:
+            print("Number of columns in Data Frame: ", rows)
+            colendcount += 1
+    rows += 1
+    Data_Frame.append(row)
+    var = int(float(Data_Frame[rows-1][0]))
+    Data_Frame1.append(var)
+    
+    
+print("Number of rows in Data Frame: ",rows)
+#var = Data_Frame[1][0]
+#print(var, type(var))    
+
+#dribble_list = [80,70,60,40,33,22,5,9]
+dribble_list = Data_Frame1
+#print(dribble_list)
+rangel = len(dribble_list)
+#print(rangel)
+
 a = 0
 maximum = 0
 while(a<rangel):
     maximum = maximum > dribble_list[a] and maximum or dribble_list[a]
     a += 1 
-#print("max: ", maximum)
+print("max: ", maximum)
 temp = maximum
 a = 0
 count = 0
@@ -30,7 +57,7 @@ while a < rangel:
                     #print("dribble_list[count-1], dribble_list[b]: ",dribble_list[count-1], dribble_list[b])
             if (count > (rangel-2)):
                 count = 0    
-            temp = 100
+            temp = maximum + 100
         else:
             #print ("rows, ", rows)
             #print("dribble_list[rows], dribble_list[rows+1]: ",dribble_list[rows], dribble_list[rows+1])
